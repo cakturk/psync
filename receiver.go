08a30b4b71f89f130acb840bd61c8f188964fb24
Cihangir Akturk <cakturk@gmail.com>
2021-01-19 18:23:23 +0300

    separate psync.go into multiple files

---
+++ receiver.go
@@ -0,0 +1,242 @@
+package main
+
+import (
+	"bytes"
+	"crypto/md5"
+	"errors"
+	"fmt"
+	stdadler32 "hash/adler32"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+type ReceiverSrcFile struct {
+	SrcFile
+
+	// following fields are not serialized
+	dstFileSize int64
+	chunkSize   int // used by receiver only
+}
+
+type Receiver struct {
+	root     string
+	srcFiles []ReceiverSrcFile
+	dec      Decoder
+}
+
+func (r *Receiver) buildFile() error {
+	var fd FileDesc
+	if err := r.dec.Decode(&fd); err != nil {
+		return err
+	}
+	if fd.ID < 0 && fd.ID > len(r.srcFiles) {
+		return fmt.Errorf("there is no such file with id: %d", fd.ID)
+	}
+	// handle new file scenario do io.Copy or something like that
+	if fd.Typ == NewFile {
+		return r.create(&r.srcFiles[fd.ID])
+	}
+	if fd.Typ != PartialFile {
+		return fmt.Errorf("unrecognized file descriptor type: %v", fd.Typ)
+	}
+	tmp, err := ioutil.TempFile("", "psync*.tmp")
+	if err != nil {
+		return err
+	}
+	defer tmp.Close()
+	defer os.Remove(tmp.Name())
+	s := &r.srcFiles[fd.ID]
+	f, err := os.Open(filepath.Join(r.root, s.Path))
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	if err = r.merge(s, f, tmp); err != nil {
+		return err
+	}
+	if err := os.Rename(tmp.Name(), f.Name()); err != nil {
+		return err
+	}
+	return os.Chtimes(f.Name(), s.Mtime, s.Mtime)
+}
+
+func (r *Receiver) merge(s *ReceiverSrcFile, rd io.ReaderAt, tmp io.Writer) error {
+	sum := md5.New()
+	tmp = io.MultiWriter(tmp, sum)
+	var off int64
+	for off < s.Size {
+		var typ BlockType
+		if err := r.dec.Decode(&typ); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		switch typ {
+		case LocalBlockType:
+			var lb LocalBlock
+			if err := r.dec.Decode(&lb); err != nil {
+				return err
+			}
+			if off != lb.Off {
+				return fmt.Errorf("local bad file offset: want %d, got: %d", lb.Off, off)
+			}
+			n, err := io.CopyN(tmp, r.dec, lb.Size)
+			off += n
+			if err != nil {
+				return err
+			}
+		case RemoteBlockType:
+			var rb RemoteBlock
+			if err := r.dec.Decode(&rb); err != nil {
+				return err
+			}
+			// XXX: rb.Off is not a remote file offset. Instead it
+			// is the local, newly created file's write offset. The
+			// read offset should be something like rb.ChunkID *
+			// chunkSize.
+			// XXX: Currently, we are not using the write offset
+			// assuming all the block descriptors (RemoteBlock,
+			// LocalBlock) are received sequentially, thus we assume
+			// that the file's current write offset is the valid
+			// file offset. However, this assumption could lead to
+			// subtle errors if we send descriptors out of order.
+			if off != rb.Off {
+				return fmt.Errorf("remote bad file offset: want %d, got: %d", rb.Off, off)
+			}
+			n, err := io.Copy(
+				tmp,
+				io.NewSectionReader(
+					rd,
+					int64(rb.ChunkID*s.chunkSize),
+					int64(rb.NrChunks*s.chunkSize),
+				),
+			)
+			off += n
+			if err != nil {
+				// last block may be smaller than the others. So check
+				// the file size first to see if this is an error we can
+				// perfectly ignore.
+				if err == io.EOF && off == s.Size {
+					break
+				}
+				return err
+			}
+		default:
+			panic("should not happen")
+		}
+	}
+	// TODO: check exact file size before returning?
+	if off < s.Size {
+		return fmt.Errorf("unexpected EOF: off: %d, size: %d", off, s.Size)
+	}
+	var (
+		typ     BlockType
+		fileSum []byte
+	)
+	if err := r.dec.Decode(&typ); err != nil {
+		return err
+	}
+	if typ != FileSum {
+		return fmt.Errorf("unexpected block type: %v", typ)
+	}
+	if err := r.dec.Decode(&fileSum); err != nil {
+		log.Printf("sum: %v", fileSum)
+		return err
+	}
+	if csum := sum.Sum(nil); !bytes.Equal(csum, fileSum) {
+		return errors.New("checksum of file does not match the original")
+	}
+	return nil
+}
+
+func (r *Receiver) create(s *ReceiverSrcFile) error {
+	name := filepath.Join(r.root, s.Path)
+	f, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_TRUNC, s.Mode)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	n, err := io.Copy(f, r.dec)
+	if err != nil {
+		return err
+	}
+	if n != s.Size {
+		return fmt.Errorf(
+			"new file size mismatch: got %d, want %d",
+			n, s.Size,
+		)
+	}
+	return os.Chtimes(name, s.Mtime, s.Mtime)
+}
+
+func doChunkFile(r io.Reader, enc Encoder, blkSize int) error {
+	sum := md5.New()
+	rol := stdadler32.New()
+	w := io.MultiWriter(sum, rol)
+	var err error
+	for err == nil {
+		var n int64
+		if n, err = io.CopyN(w, r, int64(blkSize)); err != nil {
+			if err != io.EOF {
+				return err
+			}
+			if n == 0 {
+				break
+			}
+		}
+		if err := enc.Encode(BlockSum{
+			Rsum: rol.Sum32(),
+			Csum: sum.Sum(nil),
+		}); err != nil {
+			return err
+		}
+		rol.Reset()
+		sum.Reset()
+	}
+	return nil
+}
+
+func chunkFile(path string, enc Encoder, blockSize int) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return doChunkFile(f, enc, blockSize)
+}
+
+func SendDstFiles(root string, chunkSize int, list []ReceiverSrcFile, enc Encoder) error {
+	for i, v := range list {
+		path := filepath.Join(root, v.Path)
+		info, err := os.Stat(path)
+		if err != nil {
+			if os.IsNotExist(err) {
+				if err := enc.Encode(DstFile{ID: i}); err != nil {
+					return err
+				}
+				continue
+			}
+			return nil
+		}
+		if info.ModTime() == v.Mtime && info.Size() == v.Size {
+			continue
+		}
+		if err := enc.Encode(DstFile{
+			ID:        i,
+			ChunkSize: chunkSize,
+			Size:      info.Size(),
+		}); err != nil {
+			return err
+		}
+		list[i].chunkSize = chunkSize
+		list[i].dstFileSize = info.Size()
+		if err := chunkFile(path, enc, chunkSize); err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ sender.go
@@ -0,0 +1,321 @@
+package main
+
+import (
+	"bytes"
+	"crypto/md5"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"syscall"
+
+	"github.com/chmduquesne/rollinghash/adler32"
+)
+
+// SenderSrcFile is a convenience type to represent SrcFile
+// info in sender side
+type SenderSrcFile struct {
+	SrcFile
+
+	// used by sender only. This field filled with the
+	// information received from sender.
+	dst SenderDstFile
+}
+
+// SenderBlockSum is a convenience type to represent Chunk
+// info in sender side
+type SenderBlockSum struct {
+	id int // Chunk ID (index of chunk)
+	BlockSum
+}
+
+type SenderDstFile struct {
+	DstFile
+	sums map[uint32]SenderBlockSum // used by sender
+}
+
+type Sender struct {
+	enc      Encoder
+	root     string
+	srcFiles []SenderSrcFile
+}
+
+func (s *Sender) sendBlockDescs(id int, e *SenderSrcFile) error {
+	if e.Size == 0 {
+		return nil
+	}
+	f, err := os.Open(filepath.Join(s.root, e.Path))
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return sendBlockDescs(f, id, e, s.enc)
+}
+
+//
+// x x x x x x x x x x x x x x x x x x x x x x
+//         |       0     1
+// TODO: calc merge offsets, coalesce concecutive blocks into single
+// merge descriptor.
+func sendBlockDescs(r io.Reader, id int, e *SenderSrcFile, enc Encoder) error {
+	if e.dst.Size == 0 {
+		enc.Encode(FileDesc{ID: id, Typ: NewFile, TotalSize: e.Size})
+		_, err := io.Copy(enc, r)
+		return err
+	}
+	chunkSize := int64(e.dst.ChunkSize)
+	rh := adler32.New()
+	mh := md5.New()
+	sum := md5.New()
+	r = io.TeeReader(r, sum)
+	cr := NewBring(r, int(chunkSize))
+	var err error
+	ben := blockEncoder{
+		enc:           enc,
+		r:             &cr,
+		bsize:         chunkSize,
+		lastBlockID:   e.dst.LastChunkID(),
+		lastBlockSize: e.dst.LastChunkSize(),
+	}
+	enc.Encode(FileDesc{ID: id, Typ: PartialFile})
+	log.Printf("chunkSize: %d", chunkSize)
+Outer:
+	for {
+		var n int64
+		// fill in the buffer
+		rh.Reset()
+		n, err = io.CopyN(rh, &cr, chunkSize)
+		if err != nil {
+			if err != io.EOF {
+				log.Printf("0 break: %d", cr.HeadLen())
+				return err
+			}
+			if n == 0 {
+				log.Printf("1 break: %d", cr.HeadLen())
+				break
+			}
+		}
+		log.Printf("head0: %q, tail: %q, adler: %x", cr.HeadPeek(), cr.TailPeek(), rh.Sum32())
+		ch, ok := e.dst.sums[rh.Sum32()]
+		if ok {
+			log.Println("wow I feel good!")
+			mh.Reset()
+			io.CopyN(mh, cr.Tail(), chunkSize)
+			if bytes.Equal(mh.Sum(nil), ch.Csum) {
+				ben.sendRemoteBlock(ch.id)
+				continue
+			}
+			log.Println("but not sooo good")
+		}
+		for i := int64(0); i < chunkSize; i++ {
+			c, err := cr.ReadByte()
+			if err != nil {
+				if err == io.EOF {
+					log.Printf("2 break: %d", cr.HeadLen())
+					break Outer
+				}
+				log.Printf("3 break: %d", cr.HeadLen())
+				return err
+			}
+			rh.Roll(c)
+			log.Printf("%q, adler: 0x%x", c, rh.Sum32())
+			ch, ok = e.dst.sums[rh.Sum32()]
+			if !ok {
+				continue
+			}
+			mh.Reset()
+			io.CopyN(mh, cr.Tail(), chunkSize)
+			if bytes.Equal(mh.Sum(nil), ch.Csum) {
+				// block matched, send head bytes at first
+				if i > 0 {
+					err = ben.sendLocalBlock()
+					if err != nil {
+						log.Printf("4 break: %d", cr.HeadLen())
+						return err
+					}
+				}
+				ben.sendRemoteBlock(ch.id)
+				continue Outer
+			}
+		}
+		err = ben.sendLocalBlock()
+		// enc.Encode(Blob)
+		// log.Printf(
+		// 	"headlen: %d, head: %q, tail: %q",
+		// 	cr.HeadLen(), cr.buf.Bytes()[:cr.buf.Len()-cr.blockSize],
+		// 	cr.buf.Bytes()[cr.buf.Len()-cr.blockSize:],
+		// )
+		log.Printf("head: %q, tail: %q", cr.HeadPeek(), cr.TailPeek())
+		// log.Printf("headlenPost: %d, %q, written: %d", cr.HeadLen(), cr.buf.Bytes()[:cr.buf.Len()], n)
+		if err != nil {
+			log.Printf("5 break: %d", cr.HeadLen())
+			return err
+		}
+	}
+	err = ben.flush()
+	if err != nil {
+		log.Printf("6 break: %d", cr.HeadLen())
+		return err
+	}
+	log.Printf("prologue point: head: %q, tail: %q", cr.HeadPeek(), cr.TailPeek())
+	err = enc.Encode(FileSum)
+	if err != nil {
+		return err
+	}
+	err = enc.Encode(sum.Sum(nil))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type blockEncoder struct {
+	enc        Encoder
+	r          *Bring
+	bsize, off int64
+
+	lastBlockID   int
+	lastBlockSize int64
+
+	// use offsetting to make the zero value useful,
+	// so every time we use this variable we need
+	// to subtract by 1 (offset).
+	previousID      int
+	firstID         int
+	contiguousBsize int64
+}
+
+func (d *blockEncoder) findBlockSize(id int) int64 {
+	if id == d.lastBlockID {
+		return d.lastBlockSize
+	}
+	return d.bsize
+}
+
+// TODO: Occasionally tries to send 1-byte blobs.
+func (d *blockEncoder) sendLocalBlock() error {
+	if _, ok := d.prevID(); ok {
+		err := d.flushReuseChunks()
+		if err != nil {
+			return err
+		}
+	}
+	err := d.enc.Encode(LocalBlockType)
+	if err != nil {
+		return err
+	}
+	err = d.enc.Encode(LocalBlock{
+		Size: d.r.HeadLen(),
+		Off:  d.off,
+	})
+	if err != nil {
+		return err
+	}
+	n, err := io.Copy(d.enc, d.r.Head())
+	d.off += n
+	return err
+}
+
+func (d *blockEncoder) prevID() (id int, set bool) {
+	id = d.previousID - 1
+	if id >= 0 {
+		set = true
+	}
+	return
+}
+
+func (d *blockEncoder) setPrevID(id int) { d.previousID = id + 1 }
+func (d *blockEncoder) resetPrevID()     { d.setPrevID(-1) }
+
+func (d *blockEncoder) sendRemoteBlock(id int) error {
+	bsize := d.findBlockSize(id)
+	d.r.Skip(int(bsize))
+	prevID, set := d.prevID()
+	if !set {
+		d.setPrevID(id)
+		d.firstID = id
+		d.contiguousBsize = bsize
+		return nil
+	}
+	if id-prevID != 1 {
+		if err := d.flushReuseChunks(); err != nil {
+			return err
+		}
+		d.firstID = id
+	}
+	d.contiguousBsize += bsize
+	d.setPrevID(id)
+	return nil
+}
+
+func (d *blockEncoder) flushReuseChunks() error {
+	prevID, ok := d.prevID()
+	if !ok {
+		return nil
+	}
+	numChunks := prevID - d.firstID + 1
+	err := d.enc.Encode(RemoteBlockType)
+	if err != nil {
+		return nil
+	}
+	err = d.enc.Encode(RemoteBlock{
+		ChunkID:  d.firstID,
+		NrChunks: numChunks,
+		Off:      d.off,
+	})
+	d.off += d.contiguousBsize
+	d.contiguousBsize = 0
+	d.resetPrevID()
+	return err
+}
+
+func (d *blockEncoder) flush() error {
+	err := d.flushReuseChunks()
+	if err != nil {
+		return err
+	}
+	if d.r.BufferedLen() <= 0 {
+		return nil
+	}
+	err = d.enc.Encode(LocalBlockType)
+	if err != nil {
+		return err
+	}
+	err = d.enc.Encode(LocalBlock{
+		Size: d.r.BufferedLen(),
+		Off:  d.off,
+	})
+	if err != nil {
+		return err
+	}
+	n, err := io.Copy(d.enc, d.r.Buffered())
+	d.off += n
+	return err
+}
+
+func genSrcFileList(root string) ([]SrcFile, error) {
+	var list []SrcFile
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		rel, err := filepath.Rel(root, path)
+		if err != nil {
+			return err
+		}
+		list = append(list, SrcFile{
+			Path:  rel,
+			Uid:   int(info.Sys().(*syscall.Stat_t).Uid),
+			Gid:   int(info.Sys().(*syscall.Stat_t).Gid),
+			Mode:  info.Mode(),
+			Size:  info.Size(),
+			Mtime: info.ModTime(),
+		})
+		// fmt.Println(rel)
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return list, nil
+}
